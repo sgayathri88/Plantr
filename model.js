e9f05ab87c79a496ecc953dc3be5df234b7d2892
Kristin <kristinnharper@gmail.com>
2018-08-06 17:26:32 -0400

    database synced!

--- model.js
+++ model.js
@@ -3,4 +3,31 @@ const Sequelize = require('sequelize');
 const morgan = require('morgan');
 const db = new Sequelize('postgres://localhost:5432/plantr');
 
-modules.export = db;
+//modules.export = db;
+
+
+
+const Gardener= db.define('gardener', {
+    name: Sequelize.STRING,
+    age: Sequelize.INTEGER,
+});
+
+const Plot= db.define('plot', {
+    size: Sequelize.INTEGER,
+    shaded: Sequelize.BOOLEAN,
+});
+
+const Vegetable=db.define('vegetable', {
+    name: Sequelize.STRING,
+    color: Sequelize.STRING,
+    planted_on: Sequelize.DATE,
+});
+
+Plot.belongsTo(Gardener);
+Gardener.hasOne(Plot);
+Vegetable.belongsToMany(Plot, {through: 'vegetable_plot'});
+Plot.belongsToMany(Vegetable, {through: 'vegetable_plot'})
+Gardener.belongsTo(Vegetable, {as: 'favorite_vegetable'});
+
+
+module.exports = db;
